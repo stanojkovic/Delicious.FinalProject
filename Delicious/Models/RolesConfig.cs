0e1b72ae3abe888091235416686ec7f810af40e9
Саша Станојковић <sasastanojkovic@hotmail.com>
2018-06-18 11:48:29 +0200

    Styling

---
+++ Delicious/Models/RolesConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delicious.Models
+{
+    public class RolesConfig
+    {
+        public const string ADMIN = "Administrator";
+        public const string USER = "User";
+    }
+}
